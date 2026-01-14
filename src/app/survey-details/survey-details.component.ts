b65aa50eb40e6fc49a9b65047b93a9bc047be89d
Thando <thandwem045@gmail.com>
2026-01-14 12:25:16 +0200

    Updated survey-details

--- src/app/survey-details/survey-details.component.ts
+++ src/app/survey-details/survey-details.component.ts
@@ -5,6 +5,9 @@ import { Survey } from '../models/survey';
 import { loadSurvey } from '../store/actions/survey.actions';
 import { map, Observable, take } from 'rxjs';
 import { selectSurveyById } from '../store/selectors/survey.selector';
+import { selectAllResponses, selectResponsesBySurveyId } from '../store/selectors/response.selector';
+import { SurveyResponse } from '../models/response';
+import { loadResponses } from '../store/actions/response.actions';
 
 // Works on back page 
 @Component({
@@ -14,6 +17,7 @@ import { selectSurveyById } from '../store/selectors/survey.selector';
 })
 export class SurveyDetailsComponent implements OnInit {
     surveys$!: Observable<Survey | null>;
+    response$!: Observable<SurveyResponse[] | null>
     
   constructor(
     private readonly route: ActivatedRoute,
@@ -28,19 +32,27 @@ export class SurveyDetailsComponent implements OnInit {
 
      if (survey_id !== null && !Number.isNaN(survey_id)) {
       this.surveys$ = this.store.select(selectSurveyById(survey_id));
+      this.response$ = this.store.select(selectResponsesBySurveyId(survey_id))
       this.store.dispatch(loadSurvey({ id: survey_id }));
+      this.store.dispatch(loadResponses({surveyId: survey_id}))
     } else {
       console.error('Invalid survey id');
     }
   }
 
   numOfRespondents() {
-    // this.surveys$.pipe(
-    //   map(survey => survey?.response?.length ?? 0),
-    //   take(1)
-    // )
-    // .subscribe(count => {
-    //   this.respondentCount = count;
-    // });
+    this.surveys$.pipe(
+      map(survey => survey?.surveyResponse?.length ?? 0),
+      take(1)
+    )
+    .subscribe(count => {
+      this.respondentCount = count;
+    });}
 
-}}
+  // Number of people who picked what choice 
+  // Check the number of respondents for each choice 
+
+  numOfRespondentsPerChoice() {
+     
+  }
+}
